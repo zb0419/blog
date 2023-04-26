930a0d783a45cdea6a3bbe3d4eec03d5ae66883b
zb0419 <zb0419@gmail.com>
2023-04-26 18:10:21 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,7 +1,7 @@
 export const SITE_TITLE = `Air Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = '22532453@qq.com'
-export const SITE_NAME = 'jizhouyao';
+export const SITE_NAME = 'www.jizhouyao.xyz';
 export const SITE_URL = "https://www.jizhouyao.xyz";
 export const SITE_LANG = "zh-CN";
 
