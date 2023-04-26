6e02fd3e575a532e826e63dfe5f6c033b25d043e
zb0419 <zb0419@gmail.com>
2023-04-26 18:31:28 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,8 +1,8 @@
 export const SITE_TITLE = `Air Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = '22532453@qq.com'
-export const SITE_NAME = 'www.jizhouyao.xyz';
-export const SITE_URL = "https://www.jizhouyao.xyz";
+export const SITE_NAME = 'www.jizhouyao.top';
+export const SITE_URL = "https://www.jizhouyao.top";
 export const SITE_LANG = "zh-CN";
 
 // Author, used only when author is not specified on the page, 
